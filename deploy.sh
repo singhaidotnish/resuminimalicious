71e0bba6587de71a9b43a2a21c3540ba13327c0f
zarir-engineer <zarir.utube@gmail.com>
2025-07-27 22:09:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 bundle exec jekyll build
+
 cd _site
+
+# Clean up old git metadata
+rm -rf .git
+
+# Fresh git init and push
 git init
 git remote add origin https://github.com/zarir-engineer/resuminimalicious.git
 git checkout -b gh-pages
 git add .
 git commit -m "Deploy site"
 git push -f origin gh-pages
+
 cd ..
